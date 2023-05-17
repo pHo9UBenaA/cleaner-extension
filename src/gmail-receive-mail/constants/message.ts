19869e19617a4e77582e491dfb9f64d2065255cb
pHo9UBenaA <hotya0001@gmail.com>
2023-05-17 10:30:16 +0900

    update gmail-receive-mail

--- src/gmail-receive-mail/constants/message.ts
+++ src/gmail-receive-mail/constants/message.ts
@@ -1,6 +1,6 @@
-export const MESSAGE: { [key: string]: string } = {
+export const MESSAGE = {
 	start: 'Cleaner is now operational.',
 	end: 'Cleaner operation terminated.',
 	deleteConfirm: 'May I delete the email?',
 	deleteRejected: 'Extensions have been deactivated.',
-} as const;
+} as const satisfies { [key: string]: string };
--- src/prime-video-history/constants/element.ts
+++ src/prime-video-history/constants/element.ts
@@ -1,18 +1,19 @@
 export const ElementName = {
 	tabHeader: 'h3',
-} as const;
+} as const satisfies { [key: string]: string };
 
 export const ClassName = {
 	navigation: 'settings-navigation-tabs',
 	activeNavigationElement: 'dvui-tab-active',
-} as const;
+} as const satisfies { [key: string]: string };
 
 export const Text = {
 	historyTabHeader: '視聴履歴',
 	deleteTargetButton: '視聴履歴からエピソードを削除する',
-};
+} as const satisfies { [key: string]: string };
 
+const historyTab =
+	'https://www.amazon.co.jp/gp/video/settings/watch-history/ref=atv_set_watch-history';
 export const Url = {
-	historyTab:
-		'https://www.amazon.co.jp/gp/video/settings/watch-history/ref=atv_set_watch-history',
-};
+	historyTab,
+} as const satisfies { [key: string]: string };
